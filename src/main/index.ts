5d3727c906a3ab433767dc38867e3243c9182694
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2024-12-20 18:42:12 +0530

    [ ADDED ] : CONFIGURATIONS

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,6 +10,7 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
+    backgroundMaterial: "acrylic",
     ...(process.platform === "linux" ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, "../preload/index.js"),
