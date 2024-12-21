80d641ef25416ee9ca575862366fef2fe3818deb
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2024-12-21 13:04:37 +0530

    [ ADDED ] : Notes Reading

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,7 +1,11 @@
+import { GetNotes, ReadNote } from "@shared/types"
+
 declare global {
   interface Window {
     context: {
       locale: string
+      getNotes: GetNotes
+      readNote: ReadNote
     }
   }
 }
