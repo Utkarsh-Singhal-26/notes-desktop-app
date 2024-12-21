4c440ae23a1901112de002b1f5f78293efce7eb9
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2024-12-21 11:52:43 +0530

    [ ADDED ] : Markdown Editor

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,12 @@
-import { ActionButtonsRow, Content, NotePreviewList, RootLayout, Sidebar } from "@/components"
+import {
+  ActionButtonsRow,
+  Content,
+  FloatingNoteTitle,
+  MarkdownEditor,
+  NotePreviewList,
+  RootLayout,
+  Sidebar
+} from "@/components"
 
 const App = () => {
   return (
@@ -7,7 +15,10 @@ const App = () => {
         <ActionButtonsRow className="flex justify-between" />
         <NotePreviewList className="mt-3 space-y-1" />
       </Sidebar>
-      <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+      <Content className="border-l bg-zinc-900/50 border-l-white/20">
+        <FloatingNoteTitle className="pt-2" />
+        <MarkdownEditor />
+      </Content>
     </RootLayout>
   )
 }
